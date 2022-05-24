171b159d41aa77c8374e2f5ec1b4111c7ac5bc81
kasturi28 <kasturipansare2818@gmail.com>
2022-05-24 10:58:49 +0530

    UC1

---
+++ src/com/bridgelabz/Day4snakeandladder.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+import java.util.Random;
+
+public class Day4snakeandladder {
+
+    public static void main(String[] args) {
+
+        int position = 0;
+        System.out.println("Position of player = " + position);
+
+    }
+
+
+    }
